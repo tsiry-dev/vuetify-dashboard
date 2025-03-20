47d92ba059457922b0ed594358647813ce490924
Tsiry <andriamiarijaonatsiri@gmail.com>
2025-03-20 21:35:06 +0300

    Create a navbar  tab

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar flat density="compact">
+    <v-spacer></v-spacer>
+      <v-tabs>
+        <v-tab v-for="(link, index) in links" :key="index">
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+
+  <v-main>
+      <v-container fluid>
+        HELLO
+      </v-container>
+  </v-main>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const selectedTab = ref(0);
+const links = ref(['Dashboard', 'About']);
+
+</script>
